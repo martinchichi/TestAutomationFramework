892a212c0099fda501c62374424e6f2af711c695
martinchichi <martincicinelli@gmail.com>
2022-11-08 17:01:41 -0300

    first commit, proyect creation

---
+++ src/test/java/com/endava/university/firstAutomationClass.java
@@ -0,0 +1,13 @@
+package com.endava.university;
+
+import org.testng.annotations.BeforeTest;
+
+public class firstAutomationClass {
+
+        @BeforeTest
+        public void setUp(){
+            System.out.println("First test annotation");
+        }
+
+
+}
