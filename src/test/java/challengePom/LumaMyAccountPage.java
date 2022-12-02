eb4e79925b7009804cb16db9a223f77c12c6c917
guille398 <guillermo_398@hotmail.com>
2022-12-02 16:01:53 -0300

    test

--- src/test/java/challengePom/LumaMyAccountPage.java
+++ src/test/java/challengePom/LumaMyAccountPage.java
@@ -4,9 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import static org.testng.Assert.assertEquals;
 
-public class LumaMyAccountPage  extends  BasePage{
+public class LumaMyAccountPage  extends BasePage{
     WebDriver driver;
 
+
+    public LumaMyAccountPage(){
+        super.setUpBrowser(driver);
+    }
     By locatorRegisteredMessage= By.cssSelector("body.account.customer-account-index.page-layout-2columns-left:nth-child(2) div.page-wrapper:nth-child(5) main.page-main div.page.messages:nth-child(2) div:nth-child(2) div.messages div.message-success.success.message > div:nth-child(1)");
     public LumaMyAccountPage(WebDriver driver) {
         this.driver = driver;
